32a5913b783bc30cf1f3d662f3490e7b93f0073b
smdurjoy <smdurjoy963@gmail.com>
2021-09-12 17:12:12 +0600

    Add super admin

--- index.cpp
+++ index.cpp
@@ -5,10 +5,13 @@
 using namespace std;
 
 void admin();
+void super_admin();
 void user();
+void super_admin_login();
 void admin_login();
 void user_login();
 void registration(string text, string fileName, void (*login)());
+void super_admin_panel(string username);
 void admin_panel(string username);
 void user_panel(string username);
 
@@ -18,16 +21,20 @@ int main() {
     cout<<"*********************Welcome to our Banking Managment System*************************\n";
     cout<<"\nDashboard \n\n";
     for(int i=0; i<=84; i++) cout<<"-";
-    cout<<"\n\n\t\t\t1. Admin \n";
-    cout<<"\t\t\t2. User\n";
-    cout<<"\t\t\t3. Choose an option to continue: ";
+    cout<<"\n\n\t\t\t1. Super Admin \n";
+    cout<<"\t\t\t2. Admin\n";
+    cout<<"\t\t\t3. User\n";
+    cout<<"\t\t\t4. Choose an option to continue: ";
     cin>>choice;
 
     switch (choice) {
         case 1:
-            admin();
+            super_admin();
             break;
         case 2:
+            admin();
+            break;
+        case 3:
             user();
             break;
         default:
@@ -38,6 +45,30 @@ int main() {
     return 0;
 }
 
+void super_admin() {
+    int choice;
+    system("cls");
+    cout<<"*********************************Super Admin Panel*************************************\n";
+    cout<<"\nOptions \n\n";
+    for(int i=0; i<=84; i++) cout<<"-";
+    cout<<"\n\n\t\t\t1. Login \n";
+    cout<<"\t\t\t2. Go back \n";
+    cout<<"\t\t\t3. Choose an option to continue: ";
+    cin>>choice;
+
+    switch (choice) {
+        case 1:
+            super_admin_login();
+            break;
+        case 2:
+            main();
+            break;
+        default:
+            cout<<"Invalid choice";
+            super_admin();
+    }
+}
+
 void admin() {
     int choice;
     system("cls");
@@ -45,7 +76,6 @@ void admin() {
     cout<<"\nOptions \n\n";
     for(int i=0; i<=84; i++) cout<<"-";
     cout<<"\n\n\t\t\t1. Login \n";
-    cout<<"\t\t\t2. Register \n";
     cout<<"\t\t\t3. Go back \n";
     cout<<"\t\t\t4. Choose an option to continue: ";
     cin>>choice;
@@ -53,9 +83,6 @@ void admin() {
     switch (choice) {
         case 1:
             admin_login();
-            break;
-        case 2:
-            registration("Admin", "admin.txt", admin_login);
             break;
         case 3:
             main();
@@ -91,6 +118,36 @@ void user() {
         default:
             cout<<"Invalid choice";
             user();
+    }
+}
+
+void super_admin_login() {
+    string username, password, uname, pass;
+    int is_exists=0;
+    system("cls");
+    cout<<"*********************Super Admin login*************************\n\n";
+    cout<<"Enter username: ";
+    cin>>username;
+    cout<<"Enter password: ";
+    cin>>password;
+
+    ifstream input("super_admin.txt");
+
+    while (input>>uname>>pass) {
+        if(uname == username && pass == password) {
+            is_exists = 1;
+        }
+    }
+    input.close();
+
+    if (is_exists) {
+        super_admin_panel(username);
+    } else {
+        cout<<"Invalid credentials. Try again !\n";
+        cout<<"Press enter to continue\n";
+        cin.get();
+        cin.get();
+        super_admin_login();
     }
 }
 
@@ -157,12 +214,11 @@ void user_login() {
 void registration(string text, string fileName, void (*login)()) {
     string username, password;
     system("cls");
-    cout<<"********************************"<< text << " " << "Panel************************************\n";
+    cout<<"********************************"<< text << " " << "Registration************************************\n";
     cout<<"\nChoose a username and password combination \n\n";
-    for(int i=0; i<=84; i++) cout<<"-";
-    cout<<"\n\n\t\t\tEnter username: ";
+    cout<<"\n\t\tEnter username: ";
     cin>>username;
-    cout<<"\t\t\tEnter password: ";
+    cout<<"\t\tEnter password: ";
     cin>>password;
 
     ofstream reg(fileName, ios::app);
@@ -180,9 +236,10 @@ void registration(string text, string fileName, void (*login)()) {
 void admin_panel(string username) {
     int choice;
     system("cls");
-    cout<<"*********************Admin panel*************************\n\n";
-    cout << "Welcome to the admin portal " << username << ".\n";
-    cout << "Options-> \n";
+    cout<<"*********************Admin Panel*************************\n\n";
+    cout << "Good evening " << username << ".\n\n";
+    for(int i=0; i<=56; i++) cout<<"~";
+    cout << "\n\nOptions-> \n";
     cout<<"\t1. Account holders list \n";
     cout<<"\t2. Find account \n";
     cout<<"\t3. Close or delete account \n";
@@ -212,9 +269,10 @@ void admin_panel(string username) {
 void user_panel(string username) {
     int choice;
     system("cls");
-    cout<<"*********************User panel*************************\n\n";
-    cout << "Welcome to the user portal " << username << ".\n";
-    cout << "Options-> \n";
+    cout<<"*********************User Panel*************************\n\n";
+    cout << "Good evening " << username << ".\n\n";
+    for(int i=0; i<=56; i++) cout<<"~";
+    cout << "\n\nOptions-> \n";
     cout<<"\t1. Deposite Money \n";
     cout<<"\t2. Withdraw Money \n";
     cout<<"\t3. Transfer Money \n";
@@ -257,7 +315,42 @@ void user_panel(string username) {
     }
 }
 
+void super_admin_panel(string username) {
+    int choice;
+    system("cls");
+    cout<<"*********************Super Admin Panel*************************\n\n";
+    cout << "Good evening " << username << ".\n\n";
+    for(int i=0; i<=62; i++) cout<<"~";
+    cout << "\n\nOptions-> \n\n";
+    cout<<"\t1. Create admin \n";
+    cout<<"\t2. Account holders list \n";
+    cout<<"\t3. Find account \n";
+    cout<<"\t4. Close or delete account \n";
+    cout<<"\t5. Logout \n";
+    cout<<"\t6. Choose an option to continue: ";
+    cin>>choice;
 
+    switch (choice) {
+    case 1:
+        registration("Admin", "admin.txt", admin_login);
+        break;
+    case 2:
+        cout<<"Coming soon :)";
+        break;
+    case 3:
+        cout<<"Coming soon :)";
+        break;
+    case 4:
+        cout<<"Coming soon :)";
+        break;
+    case 5:
+        main();
+        break;
+    default:
+        cout<<"Invalid choice";
+        admin_panel(username);
+    }
+}
 
 // void login(string fileName, void (*panel)(string));
 // void login(string fileName, void (*panel)(string)) {
